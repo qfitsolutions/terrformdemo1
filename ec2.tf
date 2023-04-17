e921f754a91572c2389b090e1aad4c501c4288a6
vsm <devops.july2.2017@gmail.com>
2023-04-17 19:39:41 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 resource "aws_instance" "app_server" {
   ami           = "ami-007855ac798b5175e"
-  instance_type = "t2.micro"
+  instance_type = var.instancetype
 
   tags = {
     Name = "devdemo"
