fb19b524c55f4714c18e10c6cee87b3781ca4485
Ravi <ravi@vytec.com>
2023-04-28 15:32:49 +0530

    updated

--- elb/main.tf
+++ elb/main.tf
@@ -64,3 +64,14 @@ resource "aws_lb_target_group_attachment" "test2" {
   target_id        = aws_instance.web[1].id
   port             = 80
 }
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.collectlb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.test.arn
+  }
+}
