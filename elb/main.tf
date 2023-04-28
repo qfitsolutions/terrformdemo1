54aeda3de3b03289f6b4ac15656eece0e6e0a9d2
Ravi <ravi@vytec.com>
2023-04-28 14:57:36 +0530

    added sg,sunbnets

--- elb/main.tf
+++ elb/main.tf
@@ -2,8 +2,8 @@ resource "aws_lb" "collectlb" {
   name               = "collect-lb-tf"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
+  security_groups    = ["sg-04086a1fcf30c383a"]
+  subnets            = ["subnet-0f2e6ce6044d06789","subnet-024b306dc09b49de2","subnet-04c716179f0eeb783","subnet-0a34b1e39d7084972","subnet-0e01cf6ca7bee4173","subnet-0e88e8746d4a489b6"]
 
   enable_deletion_protection = true
 
